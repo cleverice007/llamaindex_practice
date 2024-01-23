7bdde32032eb7a33d3862ecd92737b6046b2d618
cleverice007 <cleverice90128@gmail.com>
2024-01-23 17:11:39 +0800

    設定input prompt and response

--- main.py
+++ main.py
@@ -35,3 +35,42 @@ def get_index() -> VectorStoreIndex:
     return VectorStoreIndex.from_vector_store(
         vector_store=vector_store, service_context=service_context
     )
+
+st.set_page_config(
+    page_title="Chat with LlamaIndex docs, powered by LlamaIndex",
+    page_icon="🦙",
+    layout="centered",
+    initial_sidebar_state="auto",
+    menu_items=None,
+)
+
+st.title("Chat with LlamaIndex docs 💬🦙")
+
+if "messages" not in st.session_state.keys():
+    st.session_state.messages = [
+        {
+            "role": "assistant",
+            "content": "Ask me a question about LlamaIndex's open source python library?",
+        }
+    ]
+
+
+if prompt := st.chat_input("Your question"):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.write(message["content"])
+
+if st.session_state.messages[-1]["role"] != "assistant":
+    with st.chat_message("assistant"):
+        with st.spinner("Thinking..."):
+            response = st.session_state.chat_engine.chat(message=prompt)
+            st.write(response.response)
+            nodes = [node for node in response.source_nodes]
+            for col, node, i in zip(st.columns(len(nodes)), nodes, range(len(nodes))):
+                with col:
+                    st.header(f"Source Node {i+1}: score= {node.score}")
+                    st.write(node.text)
+            message = {"role": "assistant", "content": response.response}
+            st.session_state.messages.append(message)
